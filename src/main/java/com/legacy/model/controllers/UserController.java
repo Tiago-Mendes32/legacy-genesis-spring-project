eaf42983c79b9c42b419cc86700716e0795e6269
Tiago Mendes <tiago.mendes.goes@gmail.com>
2024-03-12 19:30:55 -0300

    UserController create

---
+++ src/main/java/com/legacy/model/controllers/UserController.java
@@ -0,0 +1,24 @@
+package com.legacy.model.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.legacy.model.entities.User;
+import com.legacy.model.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+	@Autowired
+	private UserService service;
+	
+	@GetMapping
+	public List<User> findAll() {
+		return service.findAll();
+	}
+}
