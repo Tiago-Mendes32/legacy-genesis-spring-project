8636ef8df8601032683aa2dccce36df7baee750f
Tiago Mendes <tiago.mendes.goes@gmail.com>
2024-03-12 19:33:19 -0300

    First test complete

--- src/main/java/com/legacy/config/InstanciationTest.java
+++ src/main/java/com/legacy/config/InstanciationTest.java
@@ -1,16 +1,22 @@
 package com.legacy.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
 
 import com.legacy.model.entities.User;
+import com.legacy.model.repositories.UserRepository;
 
-
+@Configuration
 public class InstanciationTest implements CommandLineRunner{
 
+	@Autowired
+	private UserRepository userRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		User user1 = new User(null, "Tiago", "Mendes de Góes", "50434220841", "tiagogoes75@gmail.com", "32364893");
-		
+	
+	userRepository.save(user1);
 	}
-
 }
