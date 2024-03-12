96c21e7f322ef989a30227f62a9c662ef7a5ccc2
Tiago Mendes <tiago.mendes.goes@gmail.com>
2024-03-12 19:31:38 -0300

    UserRepository create

---
+++ src/main/java/com/legacy/model/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.legacy.model.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.legacy.model.entities.User;
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+}
