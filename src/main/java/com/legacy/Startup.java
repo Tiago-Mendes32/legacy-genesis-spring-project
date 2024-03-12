1c071cb579b302c422a2653bb10a41f1d50d3307
Tiago-Mendes32 <tiago.mendes.goes@gmail.com>
2024-03-12 14:50:05 -0300

    First properties create

--- src/main/java/com/legacy/Startup.java
+++ src/main/java/com/legacy/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LegacyGenesisSpringProjectApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LegacyGenesisSpringProjectApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
